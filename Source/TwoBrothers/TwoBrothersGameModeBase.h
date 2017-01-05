3b47da1bb325ad76f4eb0be6ec90143f2cda324e
Rhyse Bendell <rhyse912@mit.edu>
2017-01-05 17:20:15 -0500

    merge with TwoBrothers

---
+++ Source/TwoBrothers/TwoBrothersGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TwoBrothersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWOBROTHERS_API ATwoBrothersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
